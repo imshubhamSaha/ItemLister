81cc4292b828b95a274761f3580ba974fe7973e1
imshubhamSaha <imshubhamsaha@gmail.com>
2023-02-20 04:41:17 +0530

    newFeature added

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const form = document.getElementById("addForm");
 const itemList = document.getElementById("items");
+const filter = document.getElementById("filter");
 
 // Form submit event
 form.addEventListener("submit", addItem);
 // Delete event
 itemList.addEventListener("click", removeItem);
+// Filter event
+filter.addEventListener("keyup", filterItems);
 
 // Add item
 function addItem(e) {
@@ -12,13 +15,19 @@ function addItem(e) {
 
   // Get input value
   const newItem = document.getElementById("item").value;
-
+  const newItem2 = document.getElementById("item2").value;
+  //Create new paragraph
+  const para = document.createElement("p");
+  // Add text node with input value
+  para.appendChild(document.createTextNode(newItem2));
   // Create new li element
   const li = document.createElement("li");
   // Add class
   li.className = "list-group-item";
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+
+  // li.appendChild(para);
 
   // Create del button element
   const deleteBtn = document.createElement("button");
@@ -39,6 +48,8 @@ function addItem(e) {
   //Append edit button to li
   li.appendChild(editBtn);
 
+  li.appendChild(para);
+
   // Append li to list
   itemList.appendChild(li);
 }
@@ -51,4 +62,27 @@ function removeItem(e) {
       itemList.removeChild(li);
     }
   }
+}
+
+// Filter Items
+function filterItems(e) {
+  // convert text to lowercase
+  const text = e.target.value.toLowerCase();
+  // Get list
+  const items = itemList.getElementsByTagName("li");
+
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    const itemName = item.firstChild.textContent;
+    const itemName2 = item.lastChild.textContent;
+
+    if (
+      itemName.toLowerCase().indexOf(text) != -1 ||
+      itemName2.toLowerCase().indexOf(text) != -1
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
